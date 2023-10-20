6f3dd9622186a96e6b120ed91861e3999c9aa5d4
Jessie1331 <141692878+Jessie1331@users.noreply.github.com>
2023-10-19 19:58:29 -0700

    Add files via upload

---
+++ 108 python 1/test.py
@@ -0,0 +1,30 @@
+    
+print('Hello World!')
+#comment#
+#in js let varname = value
+# js ;
+#python no ; needed  
+#in python #
+temperature = 20
+#string 
+name = 'jesenia'
+#number
+number = 123
+#or Easly change to boolean
+number = True
+# arrays in python ar called list
+list = [1,2,3,4,5]
+
+#issue with python is its slow
+#addition
+result = 1 + 1
+#subtraction
+result = 1 - 1
+#multiplication
+result = 1 * 1
+ #function nameOf()
+ #print is like console log
+ def greet(name)
+    Print(f "hello, {name}!")
+
+    greet("Jessie")
